6e253959d50b9cfb5f9fabda7428102369553609
Anastasios Benos <tasos24gnr95@gmail.com>
2026-07-29 21:46:03 +0200

    feat(infra): add OCI provider config and terraform lock file

---
+++ infra/provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.5"
+
+  required_providers {
+    oci = {
+      source  = "oracle/oci"
+      version = "~> 8.0"
+    }
+  }
+}
+
+provider "oci" {
+  config_file_profile = "DEFAULT"
+}
+
